80b50f69ad8bf02e4b290a808a3b3b4304260dba
Eshaan Sowale <96010356+ESaviation019@users.noreply.github.com>
2023-02-26 21:16:48 +0530

    Add files via upload

---
+++ main3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int varA[15] = {10,20,15,7,21};
+	int n;
+	scanf("%d",&n);
+	if(n<=4){printf("%d",varA[n]);
+	
+	}
+	else{
+		printf("insert the number below or equal to 4");
+	}
+return 0;
+}
+
